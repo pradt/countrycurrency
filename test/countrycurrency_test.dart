7e4ecc64933c56ca5aa485429204bf98f25fd895
Pratheepan Thevadasan <pratheepanthevadasan@Pratheepans-MBP.thevadasan.home>
2020-07-22 10:44:15 +1000

    testing automatic publish to pub.dev

--- test/countrycurrency_test.dart
+++ test/countrycurrency_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
+//import 'package:flutter_test/flutter_test.dart';
 
-import 'package:countrycurrency/countrycurrency.dart';
+//import 'package:countrycurrency/countrycurrency.dart';
 
 void main() {
   /* test('adds one to input values', () {
